822f3ebec73b9887728e536c191353ca1095a9c9
toms74209200 <motomotomato@gmail.com>
2023-04-11 01:48:40 +0900

    :bug: fix property file

--- todo-server/src/main/java/io/github/toms74209200/config/ServerConfig.java
+++ todo-server/src/main/java/io/github/toms74209200/config/ServerConfig.java
@@ -2,5 +2,5 @@ package io.github.toms74209200.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("server")
+@ConfigurationProperties("server-config")
 public record ServerConfig(String domain) {}
